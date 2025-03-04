3e9012089b39a73fc00ba8d0d24c6a5eccefdd6e
Mickael Croquet <mickael.croquet@axa.fr>
2025-03-04 14:08:09 +0100

    feat(init): Ajout du projet en mode init

---
+++ MyApi/MyApi.WebApi.Tests/Hooks/InitWebApplicationFactory.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using TechTalk.SpecFlow;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using BoDi;
+using Respawn;
+
+namespace MyApi.WebApi.Tests.Hooks;
+
+[Binding]
+internal class InitWebApplicationFactory
+{
+    internal const string HttpClientKey = nameof(HttpClientKey);
+    internal const string ApplicationKey = nameof(ApplicationKey);
+
+    [BeforeScenario]
+    public async Task BeforeScenario(ScenarioContext scenarioContext, IObjectContainer objectContainer)
+    {
+        await InitializeRespawnAsync();
+
+        var application = new WebApplicationFactory<Program>()
+            .WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureTestServices(services =>
+                {
+                    ReplaceLogging(services);
+                    ReplaceDatabase(services, objectContainer);
+                });
+            });
+
+        var client = application.CreateClient();
+
+        scenarioContext.TryAdd(HttpClientKey, client);
+        scenarioContext.TryAdd(ApplicationKey, application);
+    }
+
+    [AfterScenario]
+    public void AfterScenario(ScenarioContext scenarioContext)
+    {
+        if (scenarioContext.TryGetValue(HttpClientKey, out var client) && client is IDisposable disposable)
+        {
+            disposable.Dispose();
+        }
+
+        if (scenarioContext.TryGetValue(ApplicationKey, out var application) && application is IDisposable disposableApplication)
+        {
+            disposableApplication.Dispose();
+        }
+    }
+
+    private static void ReplaceLogging(IServiceCollection services)
+    {
+        services.RemoveAll(typeof(ILogger<>));
+        services.RemoveAll<ILogger>();
+        services.AddSingleton(typeof(ILogger<>), typeof(NullLogger<>));
+    }
+
+    private void ReplaceDatabase(IServiceCollection services, IObjectContainer objectContainer)
+    {
+        services.RemoveAll<DbContextOptions<WeatherContext>>();
+        services.RemoveAll<WeatherContext>();
+
+        services.AddDbContext<WeatherContext>(options =>
+            options.UseSqlServer(DatabaseHook.MsSqlContainer.GetConnectionString(), providerOptions =>
+            {
+                providerOptions.EnableRetryOnFailure();
+            }));
+
+        var database = new WeatherContext(new DbContextOptionsBuilder<WeatherContext>()
+            .UseSqlServer(DatabaseHook.MsSqlContainer.GetConnectionString())
+            .Options);
+
+        objectContainer.RegisterInstanceAs(database);
+    }
+
+    private async Task InitializeRespawnAsync()
+    {
+        var respawner = await Respawner.CreateAsync(
+            DatabaseHook.MsSqlContainer.GetConnectionString(),
+            new()
+            {
+                DbAdapter = DbAdapter.SqlServer
+            });
+
+        await respawner.ResetAsync(DatabaseHook.MsSqlContainer.GetConnectionString());
+    }
+}
