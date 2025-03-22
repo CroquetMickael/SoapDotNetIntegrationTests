5e93637e2b9ef9a6b7084ece8dcfccf92fecf1dc
Mickael Croquet <mickael.croquet@laposte.net>
2025-03-22 11:29:00 +0100

    feat(module4): solution module 4

--- MyApi/MyApi.WebApi.Tests/Hooks/InitWebApplicationFactory.cs
+++ MyApi/MyApi.WebApi.Tests/Hooks/InitWebApplicationFactory.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using BoDi;
 using Respawn;
+using MyApi.WebApi.Services;
+using Microcks.Testcontainers;
 
 namespace MyApi.WebApi.Tests.Hooks;
 
@@ -16,42 +18,19 @@ internal class InitWebApplicationFactory
 {
     internal const string HttpClientKey = nameof(HttpClientKey);
     internal const string ApplicationKey = nameof(ApplicationKey);
+    internal Uri _microcksUrl;
+    private MicrocksContainer _microcksContainer = null!;
 
-    [BeforeScenario]
-    public async Task BeforeScenario(ScenarioContext scenarioContext, IObjectContainer objectContainer)
+
+    private async Task CreateApiTestcontainer()
     {
-        await InitializeRespawnAsync();
-
-        var application = new WebApplicationFactory<Program>()
-            .WithWebHostBuilder(builder =>
-            {
-                builder.ConfigureTestServices(services =>
-                {
-                    ReplaceLogging(services);
-                    ReplaceDatabase(services, objectContainer);
-                });
-            });
-
-        var client = application.CreateClient();
-
-        scenarioContext.TryAdd(HttpClientKey, client);
-        scenarioContext.TryAdd(ApplicationKey, application);
+        _microcksContainer = new MicrocksBuilder()
+           .WithImage("quay.io/microcks/microcks-uber:1.10.0")
+            .WithMainArtifacts("Mocks\\Weather\\weatherSoapMock.xml")
+           .Build();
+        await _microcksContainer.StartAsync();
+        _microcksUrl = _microcksContainer.GetSoapMockEndpoint("WeatherSoap Mock", "1.0");
     }
-
-    [AfterScenario]
-    public void AfterScenario(ScenarioContext scenarioContext)
-    {
-        if (scenarioContext.TryGetValue(HttpClientKey, out var client) && client is IDisposable disposable)
-        {
-            disposable.Dispose();
-        }
-
-        if (scenarioContext.TryGetValue(ApplicationKey, out var application) && application is IDisposable disposableApplication)
-        {
-            disposableApplication.Dispose();
-        }
-    }
-
     private static void ReplaceLogging(IServiceCollection services)
     {
         services.RemoveAll(typeof(ILogger<>));
@@ -77,6 +56,21 @@ internal class InitWebApplicationFactory
         objectContainer.RegisterInstanceAs(database);
     }
 
+    public static void ReplaceExternalServices(IServiceCollection services, string url)
+    {
+        services.AddHttpClient<WeatherService>();
+
+        services.AddSingleton<IWeatherService>(provider => new WeatherService(provider.GetRequiredService<HttpClient>(), url));
+
+
+        //var mockApiWeather = new Mock<IWeatherService>();
+
+        //services.AddTransient(provider => mockApiWeather.Object);
+        //scenarioContext.TryAdd("weatherService", mockApiWeather);
+
+    }
+
+
     private async Task InitializeRespawnAsync()
     {
         var respawner = await Respawner.CreateAsync(
@@ -88,4 +82,43 @@ internal class InitWebApplicationFactory
 
         await respawner.ResetAsync(DatabaseHook.MsSqlContainer.GetConnectionString());
     }
+
+
+    [BeforeScenario]
+    public async Task BeforeScenario(ScenarioContext scenarioContext, IObjectContainer objectContainer)
+    {
+        await InitializeRespawnAsync();
+        await CreateApiTestcontainer();
+        var application = new WebApplicationFactory<Program>()
+            .WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureTestServices(services =>
+                {
+                    ReplaceLogging(services);
+                    ReplaceDatabase(services, objectContainer);
+                    ReplaceExternalServices(services, _microcksUrl.AbsoluteUri);
+                });
+            });
+
+
+        var client = application.CreateClient();
+
+        scenarioContext.TryAdd(HttpClientKey, client);
+        scenarioContext.TryAdd(ApplicationKey, application);
+    }
+
+    [AfterScenario]
+    public void AfterScenario(ScenarioContext scenarioContext)
+    {
+        if (scenarioContext.TryGetValue(HttpClientKey, out var client) && client is IDisposable disposable)
+        {
+            disposable.Dispose();
+        }
+
+        if (scenarioContext.TryGetValue(ApplicationKey, out var application) && application is IDisposable disposableApplication)
+        {
+            disposableApplication.Dispose();
+        }
+    }
+
 }
