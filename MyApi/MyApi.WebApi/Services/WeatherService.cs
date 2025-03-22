f6114118502d57a845ac17ab73f44fc7537ab0ab
Mickael Croquet <mickael.croquet@laposte.net>
2025-03-22 11:29:00 +0100

    feat(Module3): Ajout du module 3

---
+++ MyApi/MyApi.WebApi/Services/WeatherService.cs
@@ -0,0 +1,30 @@
+﻿using WeatherReference;
+
+namespace MyApi.WebApi.Services;
+
+public class WeatherService : IWeatherService
+{
+
+    private readonly CustomWeatherSoapClient _weatherSoapClient;
+
+    public WeatherService(HttpClient httpClient, string soapUrl)
+    {
+        _weatherSoapClient = CustomWeatherSoapClient.Create(soapUrl);
+    }
+
+
+    public async Task<WeatherReturn?> GetWeather(string codeZip)
+    {
+        try
+        {
+            var response = await _weatherSoapClient.GetCityWeatherByZIPAsync(codeZip);
+            return response;
+
+        }
+         catch (Exception ex)
+        {
+            
+        }
+        return null;
+    }
+}
